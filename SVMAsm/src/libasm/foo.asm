f7a435ef4aee60f99c9a7a6c67a1095fd89cca28
Marcin Copik <mcopik@gmail.com>
2012-12-17 12:45:30 +0100

    Implement inInterval macro prototype in findHighLow

--- SVMAsm/src/libasm/foo.asm
+++ SVMAsm/src/libasm/foo.asm
@@ -1,5 +1,99 @@
 extern _GLOBAL_OFFSET_TABLE_
 
+global findHighLow:function
+segment .data
+fHigh:	dd	10000000
+fLow:	dd	-1000000
+zeroConst: dd	0
+segment .text
+%macro	arrayToFPU 2
+	push	edx
+	mov	edx,%1
+	fld	dword[edx+4*%2]
+	pop	edx
+%endmacro
+
+%macro inInterval 4
+	push	edx
+	push	ecx
+	mov	edx,%1
+	add	edx,%3
+	cmp	%4,edx
+	jg	is
+	mov	edx,%2
+	sub	edx,%3
+	cmp	edx,%4
+	jg	is
+	jmp	isnt
+is:	mov	eax,1
+	jmp	intervalFinal
+isnt	mov	eax,0
+intervalFinal:	pop	edx
+	pop	ecx  
+%endmacro
+
+;	Input: ParallelAsmData struct
+;	Output: modifications in struct
+;	Finds High and Low alpha
+global findHighLow
+findHighLow:
+   	push    ebp	;save ebp
+	mov     ebp,esp	
+	push	ebx
+	xor	eax,eax
+	mov	edx,[ebp+8]	;structure
+	mov	ecx,[edx]	;trainDataSize
+	fld	dword [edx+16]	;load cost
+	fld	dword [edx+20]	;load error	
+	fld	dword [fHigh]	;fHigh init val
+	fld	dword [fLow]	;fLow init val
+	arrayToFPU	[edx+28],2
+;for(i = 0;i < trainDataSize)
+LOOP:	
+	add	ebx,1
+	loop	LOOP
+	sub	ebx,1
+	mov	[edx],ebx 
+	;fld	dword [edx+16]
+	;fld	dword [edx+20]
+	;comi	ST0,ST1
+	;jne	final
+	;jmp	end
+	
+final:	;fadd	dword [edx+20]
+	;fstp	dword [edx+16]
+	;mov	[edx],ebx
+	;sub	ebx,1
+	;mov	ecx, dword [fHigh]	
+	push	edx
+	push	ecx
+	fild	dword	[zeroConst]
+	fadd	ST0,ST4		;add error to low border
+	fcomi	ST0,ST1		;compare low+error > val
+	jb	is		;if ST0 < ST1 - may be in interval
+	jmp	isnt
+is:	fstp	ST0
+	;fst	dword [edx+16]
+	fld	ST4		;cost at stack top
+	fsub	ST0,ST4		;cost = cost-error
+	fcomi	ST0,ST1		;compare high-error > val
+	;fst	dword [edx+20]
+	jb	isnt		;if ST0 < ST1 - is not in interval
+	mov	eax,1
+	jmp	intervalFinal
+isnt	mov	eax,0
+intervalFinal:	pop	ecx
+	pop	edx
+	;arrayToFPU	[edx+28],2
+	;fst	dword [edx+20]
+	mov	[edx], eax
+	;fstp	ST0
+	;fst	dword [edx+16]	
+end:	pop	ebx
+	mov     esp,ebp
+	pop     ebp
+	ret
+
 
 global foo:function
 segment .data
