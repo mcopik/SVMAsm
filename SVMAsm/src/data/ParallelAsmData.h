97219c9498b314c0800edcd7f645dda626e35e64
Marcin Copik <mcopik@gmail.com>
2012-12-17 12:44:48 +0100

    Implement inInterval macro prototype in findHighLow

---
+++ SVMAsm/src/data/ParallelAsmData.h
@@ -0,0 +1,72 @@
+#ifndef PARALLEL_ASM_DATA_H
+#define PARALLEL_ASM_DATA_H
+
+#include "../kernel/AbstractKernel.h"
+
+struct ParallelAsmData {
+public:
+	/**
+	 * Size of arrays.
+	 */
+	unsigned int trainDataSize;
+	/**
+	 * Thread saves here iHigh value.
+	 */
+	unsigned int iHigh;
+	/**
+	 * Thread saves here iLow value.
+	 */
+	unsigned int iLow;
+	/**
+	 * Number of this thread.
+	 * Determines part of data.
+	 */
+	unsigned int threadID;
+	/**
+	 * Cost value.
+	 */
+	float cost;
+	/**
+	 * Approximation error value.
+	 */
+	float error;
+	/**
+	 * Pointer to array with Y.
+	 * Used in finding iHigh,iLow.
+	 */
+	float * yArray;
+	/**
+	 * Pointer to array with alpha values.
+	 * Used in finding iHigh,iLow.
+	 */
+	float * alphaArray;
+	/**
+	 * Pointer to array with error function values.
+	 * Used in updating error cache.
+	 */
+	float * errorArray;
+	/**
+	 * Pointer to kernel class.
+	 */
+	AbstractKernel<float> * kernel;
+	/**
+	 * Training matrix.
+	 */
+	Matrix<float> * X;
+	/**
+	 * Matrix with cached kernel.
+	 */
+	Matrix<float> * cachedKernel;
+	/**
+	 * Value of model->Y(iHigh)*model->alphas(iHigh) - highOld.
+	 */
+	float errorUpdateHigh;
+	/**
+	 * Value of model->Y(iLow)*model->alphas(iLow) - lowOld.
+	 */
+	float errorUpdateLow;
+};
+
+
+
+#endif
