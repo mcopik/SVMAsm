9e791d2449f4dbd3f356dbf6aa3c2c22fb1ec07f
Marcin Copik <mcopik@gmail.com>
2013-01-07 02:32:58 +0100

    Add tests for third part of findHighLow function

--- SVMAsm/test/AsmFindHighLowTest.cpp
+++ SVMAsm/test/AsmFindHighLowTest.cpp
@@ -1,5 +1,5 @@
 /*
- * AsmTest.cpp
+ * AsmFindHighLowTest.cpp
  *
  *  Created on: Dec 31, 2012
  *      Author: mcopik
@@ -37,7 +37,7 @@ protected:
 		}
 	}
 	virtual void TearDown() {
-		//dlclose(asmHandle);
+		dlclose(asmHandle);
 		delete threadsAsmData;
 		delete threadID;
 		delete ret;
@@ -153,5 +153,62 @@ TEST_F(AsmFindHighLowTest,testAlpha0) {
 	for(int i = 0;i < numberOfThreads;++i) {
 		ASSERT_EQ(threadsAsmData[0].iHigh,2);
 		ASSERT_EQ(threadsAsmData[i].iLow,3);
+	}
+}
+
+TEST_F(AsmFindHighLowTest,testAlphaCost) {
+	//check iHigh
+	float yArray1[] = {-1,-1,-1,-1};
+	float alphaArray1[] = {0.1,0.0991234,0.0999999,0.1};
+	float errorArray1[] = {0.0,1.14,0.5,2.5};
+	for(int i = 0;i < numberOfThreads;++i) {
+		threadsAsmData[i].trainDataSize = 4;
+		threadsAsmData[i].yArray = yArray1;
+		threadsAsmData[i].alphaArray = alphaArray1;
+		threadsAsmData[i].errorArray = errorArray1;
+	}
+	for(int i = 0;i < numberOfThreads;++i) {
+		pthread_create(&(threadID[i]),NULL,(void* (*)(void*))findHighLow,(void*)(&threadsAsmData[i]));
+		pthread_join(threadID[i],nullptr);
+	}
+	for(int i = 0;i < numberOfThreads;++i) {
+		ASSERT_EQ(threadsAsmData[0].iHigh,0);
+		ASSERT_EQ(threadsAsmData[i].iLow,-1);
+	}
+	//check iLow
+	float yArray2[] = {1,1,1,1};
+	float alphaArray2[] = {0.1,0.1,0.099,0.0993};
+	float errorArray2[] = {1.23,0.093,2.095,0.025};
+	for(int i = 0;i < numberOfThreads;++i) {
+		threadsAsmData[i].trainDataSize = 4;
+		threadsAsmData[i].yArray = yArray2;
+		threadsAsmData[i].alphaArray = alphaArray2;
+		threadsAsmData[i].errorArray = errorArray2;
+	}
+	for(int i = 0;i < numberOfThreads;++i) {
+		pthread_create(&(threadID[i]),NULL,(void* (*)(void*))findHighLow,(void*)(&threadsAsmData[i]));
+		pthread_join(threadID[i],nullptr);
+	}
+	for(int i = 0;i < numberOfThreads;++i) {
+		ASSERT_EQ(threadsAsmData[0].iHigh,-1);
+		ASSERT_EQ(threadsAsmData[i].iLow,2);
+	}
+	//check iLow and iHigh
+	float yArray3[] = {1,-1,1,-1,1,-1};
+	float alphaArray3[] = {0.1,0.0991,0.1,0.1,0.099999999,0.099331221};
+	float errorArray3[] = {1.73,1.15,2.031,2.5,0.82,0.0};
+	for(int i = 0;i < numberOfThreads;++i) {
+		threadsAsmData[i].trainDataSize = 6;
+		threadsAsmData[i].yArray = yArray3;
+		threadsAsmData[i].alphaArray = alphaArray3;
+		threadsAsmData[i].errorArray = errorArray3;
+	}
+	for(int i = 0;i < numberOfThreads;++i) {
+		pthread_create(&(threadID[i]),NULL,(void* (*)(void*))findHighLow,(void*)(&threadsAsmData[i]));
+		pthread_join(threadID[i],nullptr);
+	}
+	for(int i = 0;i < numberOfThreads;++i) {
+		ASSERT_EQ(threadsAsmData[0].iHigh,5);
+		ASSERT_EQ(threadsAsmData[i].iLow,2);
 	}
 }
